2107b61bafb49ab815ecc3a0c59adc89cdb9ec0b
Alexandre Silva <alexandre.silva@hellosaks.com>
2023-11-22 16:54:06 -0400

    feat: setup and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CommentsComponent } from './posts/comments/comments.component';
+import { PostsComponent } from './posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/:id/comments', component: CommentsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
