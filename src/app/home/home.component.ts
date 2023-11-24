e96cd000a43e0335c39c7ade17351355b4924a08
Alexandre Silva <alexandre.silva@hellosaks.com>
2023-11-24 18:42:51 -0400

    feat: home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  searchString = '';
 
+  onSearch() {
+    console.log(this.searchString);
+  }
 }
